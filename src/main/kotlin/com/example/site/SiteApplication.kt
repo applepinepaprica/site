0cdbf84f2715f8e9a1e25acbd7599787b8110cfe
applepinepaprica <applepinepaprica@gmail.com>
2019-01-20 15:12:22 +0400

    The project created

---
+++ src/main/kotlin/com/example/site/SiteApplication.kt
@@ -0,0 +1,12 @@
+package com.example.site
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SiteApplication
+
+fun main(args: Array<String>) {
+	runApplication<SiteApplication>(*args)
+}
+
