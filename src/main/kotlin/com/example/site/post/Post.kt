0bda5c90cedd116feaec4a817a995a66a3182cae
applepinepaprica <applepinepaprica@gmail.com>
2019-01-21 07:42:58 +0000

    Added Date to Post

--- src/main/kotlin/com/example/site/post/Post.kt
+++ src/main/kotlin/com/example/site/post/Post.kt
@@ -2,10 +2,12 @@ package com.example.site.post
 
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.annotation.Id
+import java.util.*
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Size
 
 @Document
 data class Post(@Id val id: String? = null,
                 @NotBlank @Size(max = 60) val title: String,
-                @NotBlank @Size(max = 6000) val text: String)
+                @NotBlank @Size(max = 6000) val text: String,
+                val createdAt: Date = Date())
