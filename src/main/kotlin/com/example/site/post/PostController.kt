aa5306e1129c18070309da392206522cde061fe2
applepinepaprica <applepinepaprica@gmail.com>
2019-01-21 08:08:53 +0000

    Added showAllPosts and updatePost

--- src/main/kotlin/com/example/site/post/PostController.kt
+++ src/main/kotlin/com/example/site/post/PostController.kt
@@ -2,12 +2,17 @@ package com.example.site.post
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import javax.validation.Valid
 
 @RequestMapping("/controller")
 @RestController
 class PostController(private val postHandler: PostHandler) {
+
+    @RequestMapping("/posts", method = [RequestMethod.GET])
+    fun showAllPosts(): Flux<Post> =
+        postHandler.showAllPosts()
 
     @RequestMapping("/post/{id}", method = [RequestMethod.GET])
     fun showPost(@PathVariable id: String): Mono<ResponseEntity<Post>> =
@@ -16,4 +21,9 @@ class PostController(private val postHandler: PostHandler) {
     @RequestMapping("/post", method = [RequestMethod.POST])
     fun addPost(@Valid @RequestBody post: Post): Mono<Post> =
             postHandler.addPost(post)
+
+    @RequestMapping("/post/{id}", method = [RequestMethod.PUT])
+    fun updatePost(@PathVariable id: String,
+                   @Valid @RequestBody post: Post): Mono<ResponseEntity<Post>> =
+        postHandler.updatePost(id, post)
 }
--- src/main/kotlin/com/example/site/post/PostHandler.kt
+++ src/main/kotlin/com/example/site/post/PostHandler.kt
@@ -2,10 +2,14 @@ package com.example.site.post
 
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Component
 class PostHandler (private val postRepository: PostRepository) {
+
+    fun showAllPosts(): Flux<Post> =
+            postRepository.findAll()
 
     fun showPost(id: String): Mono<ResponseEntity<Post>> =
             postRepository.findById(id)
@@ -14,4 +18,10 @@ class PostHandler (private val postRepository: PostRepository) {
 
     fun addPost(post: Post): Mono<Post> =
             postRepository.save(post)
+
+    fun updatePost(id: String, post: Post): Mono<ResponseEntity<Post>> =
+        postRepository.findById(id)
+                    .flatMap {postRepository.save(it.copy(title = post.title, text = post.text))}
+                    .map{ResponseEntity.ok(it)}
+                    .defaultIfEmpty(ResponseEntity.notFound().build())
 }
